69feb72d2d67c5ffb5046cac699919151cf839ad
Shmuel <shmuel>
2025-09-21 12:58:46 -0700

    Initial commit

---
+++ textgame.cpp
@@ -0,0 +1,332 @@
+#include "textgame.h"
+#include <stdlib.h>
+
+const Color3i WHITE(5, 5, 5);
+const Color3i RED(5, 0, 0);
+const Color3i GREEN(0, 5, 0);
+const Color3i BLUE(0, 0, 5);
+const Color3i BLACK(0, 0, 0);
+const Color3i CYAN(0, 5, 5);
+const Color3i MAGENTA(5, 0, 5);
+const Color3i YELLOW(5, 5, 0);
+
+// Have to define these before ncurses makes them into macros
+const Key KEY_NONE = 0;
+const Key KEY_ESCAPE = 27;
+const Key KEY_TAB = 8;
+const Key KEY_ENTER = 13;
+const Key KEY_DELETE = 0512;
+const Key KEY_DOWN = 0402;
+const Key KEY_UP = 0403;
+const Key KEY_LEFT = 0404;
+const Key KEY_RIGHT = 0405;
+const Key KEY_HOME = 0406;
+const Key KEY_BACKSPACE = 0407;
+const Key KEY_F0 = 0410;
+const Key KEY_F1 = 0411;
+const Key KEY_F2 = 0412;
+const Key KEY_F3 = 0413;
+const Key KEY_F4 = 0414;
+const Key KEY_F5 = 0415;
+const Key KEY_F6 = 0416;
+const Key KEY_F7 = 0417;
+const Key KEY_F8 = 0420;
+const Key KEY_F9 = 0421;
+const Key KEY_F10 = 0422;
+
+#ifdef _MSC_VER
+
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+Vector2i terminal_size() {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    if (hConsole == INVALID_HANDLE_VALUE) {
+        fprintf(stderr, "Error getting console handle.\n");
+    }
+
+    if (! GetConsoleScreenBufferInfo(hConsole, &csbi)) {
+        fprintf(stderr, "Error getting console screen buffer info.\n");
+    }
+
+    return Vector2i(csbi.srWindow.Right - csbi.srWindow.Left + 1,
+                    csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
+}
+
+void terminal_cleanup() {
+    // Reset the terminal
+    printf("\x1b[0m\n");
+}
+
+Key terminal_key() {
+    return _kbhit();
+}
+
+#else
+
+#include <stdio.h>
+#include <ncurses.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+void terminal_init() {
+    // ncurses setup
+    initscr();
+    cbreak();
+    noecho();
+
+    // Allow KEY_n
+    keypad(stdscr, TRUE);
+    
+    // Make getch() non-blocking
+    nodelay(stdscr, TRUE);
+}
+
+
+Key terminal_key() {
+    // ncurses
+    const char c = getch();
+    return (c == ERR) ? '\0' : c;
+}
+
+
+void terminal_cleanup() {
+    // ncurses
+    endwin();
+    
+    // Reset the terminal
+    printf("\x1b[0m\n");
+}
+
+
+#if 0
+/*
+   Linux (POSIX) implementation of _kbhit().
+   Morgan McGuire, morgan@casual-effects.com
+   https://github.com/morgan3d/misc/blob/main/kbhit/kbhit.cpp
+*/
+static char _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (! initialized) {
+        // Use termios to turn off line buffering
+        termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+}
+#endif
+
+
+Vector2i terminal_size() {
+    // Do not use curses, which needs an entire signalling mechanism
+    // to detect terminal size changes
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == 0) {
+        return Vector2i(w.ws_col, w.ws_row);
+    } else {
+        return Vector2i(0, 0);
+    }
+}
+
+#endif // _MSC_VER
+
+/////////////////////////////////////////////////////////////////////////////////////////
+Vector2i operator+(const Vector2i& a, const Vector2i& b) {
+    return Vector2i(a.x + b.x, a.y + b.y);
+}
+
+Vector2i& operator+=(Vector2i& a, const Vector2i& b) {
+    a.x += b.x;
+    a.y += b.y;
+    return a;
+}
+
+Vector2i operator-(const Vector2i& a, const Vector2i& b) {
+    return Vector2i(a.x - b.x, a.y - b.y);
+}
+
+Vector2i& operator-=(Vector2i& a, const Vector2i& b) {
+    a.x -= b.x;
+    a.y -= b.y;
+    return a;
+}
+
+Vector2i operator*(const Vector2i& a, const Vector2i& b) {
+    return Vector2i(a.x * b.x, a.y * b.y);
+}
+
+Vector2i operator*(const Vector2i& a, int b) {
+    return Vector2i(a.x * b, a.y * b);
+}
+
+Vector2i& operator*=(Vector2i& a, const Vector2i& b) {
+    a.x *= b.x;
+    a.y *= b.y;
+    return a;
+}
+
+Vector2i operator/(const Vector2i& a, int b) {
+    return Vector2i(a.x / b, a.y / b);
+}
+
+Vector2i operator/(const Vector2i& a, const Vector2i& b) {
+    return Vector2i(a.x / b.x, a.y / b.y);
+}
+
+Vector2i& operator/=(Vector2i& a, const Vector2i& b) {
+    a.x /= b.x;
+    a.y /= b.y;
+    return a;
+}
+
+bool operator==(const Vector2i& a, const Vector2i& b) {
+    return a.x == b.x && a.y == b.y;
+}
+
+bool operator!=(const Vector2i& a, const Vector2i& b) {
+    return !(a == b);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void image_resize(Image& f, Vector2i new_size) {
+    free(f.data);
+    f.size = new_size;
+    f.data = (Pixel*)malloc(f.size.x * f.size.y * sizeof(Pixel));
+    image_clear(f, ' ');
+}
+
+
+void image_clear(Image& f, Pixel value) {
+    const int n = f.size.x * f.size.y;
+    Pixel* p = f.data;
+    for (int i = 0; i < n; ++i, ++p) {
+        *p = value;
+    }
+}
+
+
+void image_set(Image& f, Vector2i pix, Pixel val) {
+    if ((val.ch != '\0') &&
+        (pix.x >= 0) &&
+        (pix.y >= 0) &&
+        (pix.x < f.size.x) &&
+        (pix.y < f.size.y)) {
+        f.data[pix.x + pix.y * f.size.x] = val;
+    }
+}
+
+
+Pixel image_get(const Image& f, Vector2i pix) {
+    if ((pix.x >= 0) &&
+        (pix.y >= 0) &&
+        (pix.x < f.size.x) &&
+        (pix.y < f.size.y)) {
+        return f.data[pix.x + pix.y * f.size.x];
+    } else {
+        return Pixel('\0');
+    }
+}
+
+
+/**
+ Converts a single UTF-16 code unit (char16_t) to a UTF-8 sequence.
+ 
+ This function handles any Unicode code point in the Basic Multilingual Plane (BMP),
+ which is the range U+0000 to U+FFFF. It does not process surrogate pairs.
+ Any input in the invalid surrogate range (U+D800 to U+DFFF) is converted to
+ the UTF-8 representation of the replacement character, U+FFFD ().
+ 
+ - c16 The input char16_t character.
+ - buffer A pointer to a character array where the UTF-8 sequence
+   will be written. This buffer must be four bytes.
+*/
+static void u16_to_u8(char16_t c16, char* buffer) {
+    // 1-byte sequence for U+0000 to U+007F (ASCII)
+    // Format: 0xxxxxxx
+    if (c16 < 0x80) {
+        buffer[0] = static_cast<char>(c16);
+        buffer[1] = '\0';
+    }
+    // 2-byte sequence for U+0080 to U+07FF
+    // Format: 110yyyyy 10xxxxxx
+    else if (c16 < 0x800) {
+        buffer[0] = static_cast<char>(0xC0 | (c16 >> 6));
+        buffer[1] = static_cast<char>(0x80 | (c16 & 0x3F));
+        buffer[2] = '\0';
+    }
+    // Invalid surrogate range U+D800 to U+DFFF
+    // Replace with U+FFFD, which is EF BF BD in UTF-8
+    else if (c16 >= 0xD800 && c16 <= 0xDFFF) {
+        buffer[0] = static_cast<char>(0xEF);
+        buffer[1] = static_cast<char>(0xBF);
+        buffer[2] = static_cast<char>(0xBD);
+        buffer[3] = '\0';
+    }
+    // 3-byte sequence for U+0800 to U+FFFF (excluding surrogates)
+    // Format: 1110zzzz 10yyyyyy 10xxxxxx
+    else {
+        buffer[0] = static_cast<char>(0xE0 | (c16 >> 12));
+        buffer[1] = static_cast<char>(0x80 | ((c16 >> 6) & 0x3F));
+        buffer[2] = static_cast<char>(0x80 | (c16 & 0x3F));
+        buffer[3] = '\0';
+    }
+}
+
+
+static int color3i_to_ansi(Color3i color) {
+    return 16 + clamp(color.r, 0, 5) * 36 + clamp(color.g, 0, 5) * 6 + clamp(color.b, 0, 5);
+}
+
+
+void image_display(Image& f) {
+    // ANSI escape sequence for "clear screen and go to top"
+    printf("\033[H\033[J");
+
+    // UTF-8 conversion buffer
+    char buffer[4];
+
+    // TODO: ssprintf to a buffer, then send to screen at once
+    const Pixel* p = f.data;
+    for (int y = 0; y < f.size.y; ++y) {
+        for (int x = 0; x < f.size.x; ++x, ++p) {
+            u16_to_u8(p->ch, buffer);
+            printf("\033[38;5;%dm\033[48;5;%dm%s",
+                   color3i_to_ansi(p->fg),
+                   color3i_to_ansi(p->bg),
+                   buffer);
+        }
+        printf("\n");
+    }
+
+    #ifndef _MSC_VER
+    // Curses
+    move(0, 0);
+    refresh();
+    #endif
+    
+}
+
+
+void image_blit
+(Image& dst,
+ Vector2i dst_corner,
+ const Image& src,
+ Vector2i src_corner,
+ Vector2i size,
+ bool overwrite_bg) {
+    // TODO
+}
---
+++ textgame.h
@@ -0,0 +1,142 @@
+/*
+  Defines in a platform independent way:
+
+ */
+#pragma once
+
+#include <algorithm>
+
+template<typename T>
+T clamp(T value, T min_val, T max_val) {
+    return std::max(min_val, std::min(value, max_val));
+}
+
+using Character = char32_t;
+struct Vector2i;
+struct Color3i;
+
+/////////////////////////////////////////////////////////////////////////
+
+/* See the KEY_ constants */
+using Key = uint32_t;
+
+extern const Key KEY_NONE;
+extern const Key KEY_ESCAPE;
+extern const Key KEY_TAB;
+extern const Key KEY_ENTER;
+extern const Key KEY_DELETE;
+extern const Key KEY_DOWN;
+extern const Key KEY_UP;
+extern const Key KEY_LEFT;
+extern const Key KEY_RIGHT;
+extern const Key KEY_HOME;
+extern const Key KEY_BACKSPACE;
+extern const Key KEY_F0;
+extern const Key KEY_F1;
+extern const Key KEY_F2;
+extern const Key KEY_F3;
+extern const Key KEY_F4;
+extern const Key KEY_F5;
+extern const Key KEY_F6;
+extern const Key KEY_F7;
+extern const Key KEY_F8;
+extern const Key KEY_F9;
+extern const Key KEY_F10;
+
+
+/////////////////////////////////////////////////////////////////////////
+
+void terminal_init();
+void terminal_cleanup();
+Key terminal_key();
+Vector2i terminal_size();
+
+/////////////////////////////////////////////////////////////////////////
+
+struct Vector2i {
+    int x;
+    int y;
+
+    Vector2i() : x(0), y(0) {}
+    Vector2i(int x, int y) : x(x), y(y) {}
+};
+
+// Global operator forward declarations for Vector2i
+Vector2i operator+(const Vector2i& a, const Vector2i& b);
+Vector2i& operator+=(Vector2i& a, const Vector2i& b);
+Vector2i operator-(const Vector2i& a, const Vector2i& b);
+Vector2i& operator-=(Vector2i& a, const Vector2i& b);
+Vector2i operator*(const Vector2i& a, const Vector2i& b);
+Vector2i operator*(const Vector2i& a, int b);
+Vector2i& operator*=(Vector2i& a, const Vector2i& b);
+Vector2i operator/(const Vector2i& a, int b);
+Vector2i operator/(const Vector2i& a, const Vector2i& b);
+Vector2i& operator/=(Vector2i& a, const Vector2i& b);
+bool operator==(const Vector2i& a, const Vector2i& b);
+bool operator!=(const Vector2i& a, const Vector2i& b);
+
+///////////////////////////////////////////////////////////////////////
+
+/* ANSI color with each channel on the range 0-5 */
+struct Color3i {
+    int r;
+    int g;
+    int b;
+
+    Color3i() : r(0), g(0), b(0) {}
+    Color3i(int r, int g, int b) : r(r), g(g), b(b) {}
+};
+
+extern const Color3i WHITE;
+extern const Color3i RED;
+extern const Color3i GREEN;
+extern const Color3i BLUE;
+extern const Color3i BLACK;
+extern const Color3i CYAN;
+extern const Color3i MAGENTA;
+extern const Color3i YELLOW;
+
+////////////////////////////////////////////////////////////////////////
+
+struct Pixel {
+    Color3i      fg;
+    Character    ch;
+    Color3i      bg;
+    
+    Pixel() : fg(), ch(0), bg() {}
+    Pixel(Character c) : fg(), ch(c), bg() {}
+    Pixel(Color3i f, Character c, Color3i b = BLACK) : fg(f), ch(c), bg(b) {}
+    Pixel(Color3i b) : fg(WHITE), ch(0), bg(b) {}
+};
+
+
+struct Image {
+    Vector2i    size;
+    /* Row-major */
+    Pixel*      data;
+
+    Image() : data(nullptr) {}
+    Image(Vector2i size, Pixel value = Pixel(' '));
+    ~Image() { free(data); }
+};
+
+
+void image_resize(Image& img, Vector2i new_size);
+void image_clear(Image& img, Pixel value);
+void image_set(Image& img, Vector2i pix, Pixel val);
+Pixel image_get(const Image& img, Vector2i pix);
+void image_display(Image& img);
+
+/* Blits src into dst, clipping to each and treating the specified character as transparent. 
+   If overwrite_bg is true, the bg color of src is copied to dst, otherwise the bg color
+   of each dst pixel is preserved. */
+void image_blit(Image& dst, Vector2i dst_corner, const Image& src, Vector2i src_corner, Vector2i size, bool overwrite_bg = true, Character transparent = '\0');
+
+#ifdef _MSC_VER
+// https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/kbhit?view=msvc-170
+#include <conio.h>
+
+#else
+int _kbhit();
+
+#endif
